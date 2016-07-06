6921034bde59bb7de774a652196e7d84f2826f85
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-06 16:37:10 +0530

    Create reversestrings.c

---
+++ reversestrings.c
@@ -0,0 +1,13 @@
+# playerprograms
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s,sss,n;
+printf("\n enter string");
+scanf("%s",&s");
+sss=s;
+n=strrev(s);
+printf("%s",n);
+}
