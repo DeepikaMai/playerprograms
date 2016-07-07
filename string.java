5c34d5ce4a9d1e9e77fd19799bc78ac2cb150672
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-07 22:01:48 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,10 @@
+public class StringReverse
+{
+public static void main(String args[])
+{
+String strori="hello"
+System.out.println("orig" + strori);
+strori=new StringBuffer(strOriginal).reverse().toString();
+System.out,println(strori);
+}
+}
