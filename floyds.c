3c7c7aa5853e697c278c2c7b3cd25ae736396e53
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-07 20:34:26 +0530

    Create floyds.c

---
+++ floyds.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+int i,n,c,a=1;
+printf("\n enter num");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+for(c=1;c<i;c++)
+{
+printf("\n");
+}
+return 0;
+}
