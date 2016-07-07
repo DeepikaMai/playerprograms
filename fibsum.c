3026522d1be87ab1becb1dad75109c32156ecae1
DIVYAPRABHA1602 <divyaprabha1602@gmail.com>
2016-07-07 21:02:09 +0530

    Create fibsum.c

---
+++ fibsum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,fib,sum;
+printf("\n number");
+scanf("%d",&n);
+a=0;
+b=1;
+for(i=3;i<=n;i++)
+{
+fib=a+b;
+a=b;
+b=c;
+printf("\n %d",fib);
+}
+}
