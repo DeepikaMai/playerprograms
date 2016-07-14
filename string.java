75fba6b626150bb87b754188ba77451d28565982
DeepikaMai <deepikanr29@gmail.com>
2016-07-14 11:31:50 +0530

    syntax error

--- string.java
+++ string.java
@@ -5,6 +5,6 @@ public static void main(String args[])
 String strori="hello"
 System.out.println("orig" + strori);
 strori=new StringBuffer(strOriginal).reverse().toString();
-System.out,println(strori);
+System.out.println(strori);
 }
 }
